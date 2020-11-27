1678f9421430dced22c7697696114d5ca67f547c
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-11-27 21:09:57 +0200

    Repair Task1 and Add Task2
    2. Теперь добавьте проверку переменных в генерирующую функцию и обрабатывайте её!

--- Modul15_ErrorHandling.playground/Contents.swift
+++ Modul15_ErrorHandling.playground/Contents.swift
@@ -3,7 +3,7 @@ import UIKit
 // MARK:  - Task 1
 print("--- Task #1 ---")
 
-enum ServerError: Error, CaseIterable {
+enum ServerError: Error {
     case error400
     case error404
     case error500
@@ -18,14 +18,41 @@ do {
     if badRequest {
         throw ServerError.error400
     }
-    
+
     if pageNotFound {
         throw ServerError.error404
     }
-    
+
     if pageNotFound {
         throw ServerError.error500
     }
+} catch ServerError.error400 {
+    print("Ошибка сервера: Плохой, неверный запрос")
+} catch ServerError.error404 {
+    print("Ошибка сервера: Не найдено")
+} catch ServerError.error500 {
+    print("Ошибка сервера: Внутренняя ошибка сервера")
+}
+
+// MARK:  - Task 2
+print("--- Task #2 ---")
+
+func checkServer() throws {
+    if badRequest {
+        throw ServerError.error400
+    }
+
+    if pageNotFound {
+        throw ServerError.error404
+    }
+
+    if pageNotFound {
+        throw ServerError.error500
+    }
+}
+
+do {
+    try checkServer()
 } catch ServerError.error400 {
     print("Ошибка сервера: Плохой, неверный запрос")
 } catch ServerError.error404 {
