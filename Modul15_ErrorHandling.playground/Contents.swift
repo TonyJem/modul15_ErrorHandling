959c8d54f3c8f92a2e1dc49ee2cb3bef5e7ebc48
TonyJem <62282734+TonyJem@users.noreply.github.com>
2020-11-27 20:32:34 +0200

    Add Task 1
    1. Создайте перечисление для ошибок. Добавьте в него 3 кейса:
    ошибка 400,
    ошибка 404,
    ошибка 500.
    
    Далее создайте переменную, которая будет хранить в себе какую-либо ошибку (400 или 404 или 500). И при помощи do-catch сделайте обработку ошибок перечисления. Для каждой ошибки должно быть выведено сообщение в консоль.

--- Modul15_ErrorHandling.playground/Contents.swift
+++ Modul15_ErrorHandling.playground/Contents.swift
@@ -1,3 +1,35 @@
 import UIKit
 
-var str = "Hello, playground"
+// MARK:  - Task 1
+print("--- Task #1 ---")
+
+enum ServerError: Error, CaseIterable {
+    case error400
+    case error404
+    case error500
+}
+
+// MARK: Variables
+var badRequest: Bool = false
+var pageNotFound: Bool = true
+var internalServerError: Bool = false
+
+do {
+    if badRequest {
+        throw ServerError.error400
+    }
+    
+    if pageNotFound {
+        throw ServerError.error404
+    }
+    
+    if pageNotFound {
+        throw ServerError.error500
+    }
+} catch ServerError.error400 {
+    print("Ошибка сервера: Плохой, неверный запрос")
+} catch ServerError.error404 {
+    print("Ошибка сервера: Не найдено")
+} catch ServerError.error500 {
+    print("Ошибка сервера: Внутренняя ошибка сервера")
+}
